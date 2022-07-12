5a7ec8343f9d4df89b6383ddb32b7bcc94d82145
Aden Addis <adenwonde56@gmail.com>
2022-07-12 13:55:07 -0400

    Added comment form in html and js (incomplete), changed background in css

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // //write your code here!
+
+// document.addEventListener('DOMContentLoaded', () => {
+
 // fetch("https://type.fit/api/quotes")
 //   .then(function(response) {
 //     return response.json();
@@ -6,3 +9,17 @@
 //   .then(function(data) {
 //     console.log(data);
 //   });
+
+
+const commentlist = document.getElementById('comments-list')
+
+
+
+form = document.getElementById('comment-form')
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const newComment = event.target.comment.value
+    const li = document.createElement("li")
+    li.textContent = newComment
+    commentList.append(li)
+})
