9929d24091113905738efd0dee6d58c6cfef6501
Aden Addis <adenwonde56@gmail.com>
2022-07-14 13:36:30 -0400

    not working

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
-// //write your code here!
-
-// document.addEventListener('DOMContentLoaded', () => {
-
-// fetch("https://type.fit/api/quotes")
-//   .then(function(response) {
-//     return response.json();
-//   })
-//   .then(function(data) {
-//     console.log(data);
-//   });
+document.addEventListener('DOMContentLoaded', () => {
 
 
-const commentlist = document.getElementById('comments-list')
 
-form1 = document.getElementById('comment-form')
-form1.addEventListener('submit', (event) => {
+const commentList = document.getElementById("dinosaur-monster")
+console.log(commentList);
+ const form = document.getElementById("comment-form");
+ console.log(form)
+
+ form.addEventListener("submit", (event) => {
     event.preventDefault()
-    const newComment1 = event.target.comment.value
-    const li = document.createElement("li")
-    li.textContent = newComment1
-    commentList.append(li)
-    console.log(form1)
+    console.log("hello")
+ })
+    // console.log("hello")
+    // console.log(event.target.comment.value)
+
+    // const newComment = (event.target.comment.value)
+    // const li = document.createElement("li")
+    // li.textContent = newComment
+    // commentList.append(li)
+
+    // console.log(form)
+// })
+// const commentlist = document.getElementById('comments-list')
+// const form = document.getElementById('comment-form1')
+// form.addEventListener('submit', (event) => {
+//     event.preventDefault()
+//     const newComment1 = event.target.comment.value
+//     const li = document.createElement("li")
+//     li.textContent = newComment1
+//     commentlist.append(li)
+// })
+
+const likeCount = document.getElementById("like-count")
+
+const likeButton = document.getElementById("like-button")
+likeButton.addEventListener('click', () => {
+    likeCount.innertext = likeCount.dataset.likeCount + "likes"
 })
 
-form2 = document.getElementById('comment-form2')
-form2.addEventListener('submit', (event) => {
-    event.preventDefault()
-    const newComment2 = event.target.comment.value
-    const li = document.createElement("li")
-    li.textContent = newComment2
-    commentList.append(li)
 })
+
